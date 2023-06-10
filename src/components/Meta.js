785ead892987e6b4e747180f514274a339d2dee0
Neeman Elchurafa <neemanchurafa@gmail.com>
2023-06-09 23:14:02 -0500

    added meta tag for seo. will follow up other lighthouse pagespeed insights

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -1,10 +1,14 @@
 import Head from "next/head";
 
 export default function Meta() {
-    return (
-        <Head>
-         <title>ProtoCall Development</title>
-         <link rel="icon" href="/favicon.ico" />
-        </Head>
-    );
+  return (
+    <Head>
+      <title>ProtoCall Development</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="description"
+        content="ProtoCall Development is a software development company that specializes in building custom web and mobile applications using JavaScript, React, Next.js, and Node.js. Contact us to learn more about our services."
+      />
+    </Head>
+  );
 }
